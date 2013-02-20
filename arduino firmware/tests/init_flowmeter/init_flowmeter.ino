c19037f7eafccea5be02efd643b7c3f1469a3990
David Turner <cillian64@googlemail.com>
2013-02-20 11:04:22 +0000

    Fixed init_flowmeter test

--- arduino firmware/tests/init_flowmeter/init_flowmeter.ino
+++ arduino firmware/tests/init_flowmeter/init_flowmeter.ino
@@ -1,33 +1,33 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial mySerial(53, 9); // RX, TX
+//SoftwareSerial mySerial(53, 9); // RX, TX
 
 // Use node address 128 - flowmeter should ALWAYS listen to this
 
 void setup()  
 {
 
-  mySerial.begin(38400); // Baud specified in flowmeter docs.
-  // Apparently we have to do pinmodes manually:
+  Serial1.begin(38400); // Baud specified in flowmeter docs.
+
+  // High impedance the two softwareserial pins
   pinMode(53, INPUT);
-  pinMode(9, OUTPUT);
+  pinMode(9, INPUT);
 
   delay(5000); // Wait for the flowmeter to warm up
-  mySerial.println(":050001000A49\r\n");
-  mySerial.println(":050001000101\r\n");
-  mySerial.println(":05000100027F\r\n");
-  mySerial.println(":050001000302\r\n"); // TODO: check for
-  mySerial.println(":050001000420\r\n"); // error response to
-  mySerial.println(":050001000502\r\n"); // each of these
-  mySerial.println(":050001000A52\r\n"); // RESET!
+
+  // Init stuff:
+  Serial1.println(":050001000A49\r\n");
+  Serial1.println(":050001000101\r\n");
+  Serial1.println(":05000100027F\r\n");
+  Serial1.println(":050001000302\r\n"); // TODO: check for
+  Serial1.println(":050001000420\r\n"); // error response to
+  Serial1.println(":050001000502\r\n"); // each of these
+  
+  Serial1.println(":050001000A52\r\n"); // RESET!
   delay(2000); // GIve it a chance to reset.
   
   // Must change setpoint controlmode, process 01 parameter 12
-  // Set to 20 for "valve steering" or "18" for RS232 setpoint
-  // 3 for close valve, 8 for open valve
-  // 7  for 100%, 12 for 0%
-  // All 1 byte sent as 2byte hex
-  
+  // Set to 20 for "valve steering" which is what we want
   // :
   // length=5 = '05'
   // node=128 = '80'
@@ -36,7 +36,7 @@ void setup()
   // parameter 12 = '12'
   // value 20 = '20'
   char message[]=":058002011220\r\n";
-  mySerial.print(message);
+  Serial1.print(message);
   delay(1000);
 
 }
@@ -50,17 +50,16 @@ void loop() // run over and over
   // length = 6 = '06'
   // node = 128 = '80'
   // cmd = 02 = '02'      (parameter set with no response)
-  // process = 01 = '01' ???
+  // process = 01 = '01' ??? Don't understand a lot of these but it seems to work
   // parameter = 21 = '21' ???
   // setpoint1 = '00' or '7D'
   // setpoint2 = '00'
-  // '\r'
-  //  '\n'
+  // '\r', '\n'
 
-  sprintf(message, ":06800201210000\r\n");
-  mySerial.print(message);
+  sprintf(message, ":06800201210000\r\n"); // Valve close, 0x0000
+  Serial1.print(message);
   delay(1000);
-  sprintf(message, ":06800201217D00\r\n");
-  mySerial.print(message);
+  sprintf(message, ":06800201217D00\r\n"); // Wide open, 0x7D00
+  Serial1.print(message);
   delay(1000);
 }
