df51c22e79303a982522929fc65a6d180f8358dd
David Turner <cillian64@googlemail.com>
2013-02-20 15:18:30 +0000

    Cleaned up get_accumulation test program

--- arduino firmware/tests/get_accumulation/get_accumulation.ino
+++ arduino firmware/tests/get_accumulation/get_accumulation.ino
@@ -2,68 +2,42 @@
 
 SoftwareSerial serLCD(3, 7); // 3=RX (unused), 7=TX
 
-// Use node address 128 = 0x80 - flowmeter should ALWAYS listen to this
-
-// Protip: http://www.h-schmidt.net/FloatConverter/IEEE754.html for 4byte IEC float<=>hex
-
-// Protip2: Message lengths are number of bytes (i.e. half the number of characters,
-// we send two characters of hex per byte) not including the length byte and not inc. \r\n
-
 float read2float(char buffer[]);
-void lcdorigin()
-{
-  serLCD.write(254); // move cursor to beginning of first line
-  serLCD.write(128);
-}
-
-void lcdclear()
-{
-  lcdorigin();
-  serLCD.write("                ");
-  serLCD.write("                ");
-}
+void lcdorigin();
+void lcdclear();
+void setvalve(int percentageopen);
+float getaccumulation();
 
 void setup()  
 {
-  float accumulation=0.0;
-  
   Serial1.begin(38400); // Baud specified in flowmeter docs.
   serLCD.begin(9600);
 
-  // High impedance the two softwareserial pins
-  pinMode(53, INPUT);
+  pinMode(53, INPUT); // High impedance - these clash with hardware UART 1
   pinMode(9, INPUT);
   
-  // Setup LCD serial:
-  pinMode(3, INPUT);
+  pinMode(3, INPUT);   // LCD software serial pins
   pinMode(7, OUTPUT);
 
-  delay(3000); // Wait for the flowmeter (and LCD) to warm up
+  delay(2000); // Wait for the flowmeter (and LCD) to warm up
 
   // Init stuff:
-  // Note, using \r\n so print, not println
   Serial1.print(":050001000A49\r\n");
   Serial1.print(":050001000101\r\n");
   Serial1.print(":05000100027F\r\n");
-  Serial1.print(":050001000302\r\n"); // TODO: check for
-  Serial1.print(":050001000420\r\n"); // error response to
-  Serial1.print(":050001000502\r\n"); // each of these
+  Serial1.print(":050001000302\r\n");
+  Serial1.print(":050001000420\r\n");
+  Serial1.print(":050001000502\r\n");
   
   Serial1.print(":050001000A52\r\n"); // RESET!
-  delay(2000); // GIve it a chance to reset.
+  delay(1000); // GIve it a chance to reset.
   
-  // Must change setpoint controlmode, process 01 parameter 12
-  // Set to 20 for "valve steering" which is what we want
-  // :
-  // length=5 = '05'
-  // node=128 = '80'
-  // cmd=02 = '02'
-  // process = 01 = '01'
-  // parameter 4, type 'c' = 0x04
-  // value 20 = '14'
-  Serial1.print(":058002010414\r\n"); // Set controlmode to valve steering
+  // Must change setpoint controlmode
+  // Process 01, parameter 04 type 'c'
+  // Set to 20 for "valve steering"
+  Serial1.print(":058002010414\r\n");
 
-  // Need to set CntrMode to 1, counting upwards continuously
+  // Need to set CntrMode to counting upwards continuously
   // process 104 = 0x68
   // parameter 8 type c = 0x08
   // Set to 1 = 0x01
@@ -72,48 +46,37 @@ void setup()
   // Then reset counter to zero, i.e.
   // process 104 = 0x68
   // Parameter 1 type f = 0x01 + 0x40 = 0x41
-  // set to 0.0 = 0x00000000
+  // set to 0.0 = 0x00000000 (4 byte float)
   Serial1.print(":088002684100000000\r\n");
   
   // Blank out the LCD:
   lcdclear();
-
-
 }
 
 
-void loop() // run over and over
+void loop()
 {
   char message[128];
   char buffer[128];
+  float accumulation=-1.0;
 
-  sprintf(message, ":06800201210000\r\n"); // Valve close, 0x0000
-  Serial1.print(message);
+  setvalve(0);
   lcdclear();
   serLCD.write("valve closed");
   delay(1000);
   
-  
-  sprintf(message, ":06800201217D00\r\n"); // Wide open, 0x7D00
-  Serial1.print(message);
+  setvalve(100);
   for(int i=0; i<10; i++)
-  {
-        // Then to retrieve the accumulation:
-        // First clear our read buffer:
-        while(Serial1.read() != -1);
-        
-        // Process 104 = 0x68
-        // Parameter 1 type f = 0x01 + 0x40 = 0x41
-        Serial1.print(":06800468416841\r\n");
-        
-        // Should receive :08??026841AABBCCDD\r\n
-        // Where AABBCCDD are 4 bytes of float and ?? is flowmeter node number
-        if(Serial1.readBytesUntil('\n', buffer, 128) >= 19)
-        {
+  {        
+	// Then to retrieve the accumulation:
+	accumulation = getaccumulation();
+
+	if(accumulation >= 0.0)
+	{
            lcdclear();
            serLCD.write("valve open samp");
            serLCD.print(i);
-           serLCD.print(read2float(buffer));
+           serLCD.print(accumulation);
            delay(300);
         }
         else
@@ -153,4 +116,53 @@ float read2float(char buffer[])
   
   return accumulation;
 }
+
+void setvalve(int percentageopen)
+{
+  char message[32];
+  // Scale the open-ness from 0-100 to 0x0000-0x7D00
+  unsigned int rescale = 0x7D00 * percentageopen/100;
+  char setpoint[5];
+
+  if(rescale > 0x7D00)
+    rescale=0x0000; // Default closed;
+
+
+  sprintf(message, ":0680020121%04X\r\n", rescale); // Valve close, 0x0000
+  Serial1.print(message);
+}
+
+float getaccumulation()
+{
+	char buffer[128];
+
+	// First clear our read buffer:
+	while(Serial1.read() != -1);
+
+	// Send read counter command:       
+        // Process 104 = 0x68
+        // Parameter 1 type f = 0x01 + 0x40 = 0x41
+        Serial1.print(":06800468416841\r\n");
+        
+        // Should receive :08??026841AABBCCDD\r\n
+        // Where AABBCCDD are 4 bytes of float and ?? is flowmeter node number
+        if(Serial1.readBytesUntil('\n', buffer, 128) >= 19)
+           return read2float(buffer);
+        else
+	   return -13.37; // Fail!
+}
   
+void lcdorigin()
+{
+  serLCD.write(254); // move cursor to beginning of first line
+  serLCD.write(128);
+}
+
+void lcdclear()
+{
+  lcdorigin();
+  serLCD.write("                ");
+  serLCD.write("                ");
+}
+
+
